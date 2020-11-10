2c7e39bf1d829a22c5d77b1e4100943361e25662
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-10 11:34:10 +0000

    further progress on garage class

--- GarageProblem/src/Classes/Garage.java
+++ GarageProblem/src/Classes/Garage.java
@@ -1,24 +1,53 @@
 package Classes;
 
+import java.util.ArrayList;
+
 public class Garage {
-	//VEHICLES
-    Car ford= new Car(true, 4,"Ford Fiesta", 1976);
-    Truck dodge= new Truck(false, 4, "Dodge RAM 2500","Blue");
-    Motorbike Yamaha= new Motorbike(true, 2, "Yamaha","broken exhasut",true);
-    Car Lamborghini= new Car(false, 4, "Lamborghini Jalpa",1981);
-    Truck landRover= new Truck(false, 4, "Land Rover Defender","Black");
-    Motorbike Triumph= new Motorbike(true, 2, "XSR 900","Crushed Engine",false);
-    Car Merc = new Car(true, 4, "Mercedes A-Class",1997);
-    
-    //LIST OF VEHICLES
-    
-    //???
-    ArrayList<vehicles> vehicle = new ArrayList<>(); 
-    people.add(bob);
-    people.add(barbara);
-    people.add(scrumpo);
-    people.add(huckleberry);
-    people.add(kevin);
-    people.add(sara);
-    people.add(adam);
+	// ATTRIBUTES
+
+	protected String location;
+	protected int cost;
+	protected ArrayList<Vehicle> vehicleList = new ArrayList<>();
+	
+	// VEHICLES
+	public Garage(String location, int cost, Vehicle vehicle) {
+		
+	Vehicle fiesta = new Car(true, 4, "Ford Fiesta", 1976);
+	Vehicle dodge = new Truck(false, 4, "Dodge RAM 2500", "Blue");
+	Vehicle Yamaha = new Motorbike(true, 2, "Yamaha", "broken exhaust", true);
+	}
+	// Car Lamborghini= new Car(false, 4, "Lamborghini Jalpa",1981);//
+	// Truck landRover= new Truck(false, 4, "Land Rover Defender","Black");//
+	// Motorbike Triumph= new Motorbike(true, 2, "XSR 900","Crushed
+	// Engine",false);//
+	// Car Merc = new Car(true, 4, "Mercedes A-Class",1997);//
+
+	// METHODS
+	public void addVehicle(Vehicle vehicle) {
+		this.vehicleList.add(vehicle);
+
+	}
+
+	public void remove(int n) {
+		this.vehicleList.remove(n);
+		
+	}
+	public void removeAll() {
+		this.vehicleList.clear();
+		
+	}
+	public int fixCost(int cost, Vehicle vehicle) {
+		for()
+		if(vehicle.getWheel() ==2) {
+			cost = 200;
+		} else {
+		cost = 400;
+		}
+		return cost;
+		
+		
+		
+	}
+	
+	
 }
--- GarageProblem/src/Main/Runner.java
+++ GarageProblem/src/Main/Runner.java
@@ -1,12 +1,9 @@
 package Main;
-
-import Classes.Car;
-
+import Classes.Garage;
 public class Runner {
 	public static void main(String[] arg) {
 		
-		
-		
+		System.out.println("Hello World");
 	}
 
 }
